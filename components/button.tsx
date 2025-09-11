858900ccfee6116d4a30184d3bcdbd250257f6f1
Jijo <jijo@Jijos-MacBook-Air.local>
2025-09-11 14:13:50 +0530

    link transition fix

--- components/button.tsx
+++ components/button.tsx
@@ -1,43 +1,35 @@
-"use client";
-import React from "react";
-import { cn } from "@/lib/utils";
-import LinkTransition from "next-view-transitions";
+'use client';
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { Link } from 'next-view-transitions';
 
 export const Button = ({
   href,
-  as: Tag = "a",
+  as: Tag = 'a',
   children,
   className,
-  variant = "primary",
+  variant = 'primary',
   ...props
 }: {
   href?: string;
   as?: React.ElementType;
   children: React.ReactNode;
   className?: string;
-  variant?: "primary" | "secondary" | "dark" | "gradient";
-} & (
-  | React.ComponentPropsWithoutRef<"a">
-  | React.ComponentPropsWithoutRef<"button">
-  | typeof LinkTransition
-)) => {
+  variant?: 'primary' | 'secondary' | 'dark' | 'gradient';
+} & (React.ComponentPropsWithoutRef<'a'> | React.ComponentPropsWithoutRef<'button'> | typeof Link)) => {
   const baseStyles =
-    "px-4 py-2 rounded-md bg-white button bg-white text-black text-sm font-bold relative cursor-pointer hover:-translate-y-0.5 transition duration-200 inline-block text-center";
+    'px-4 py-2 rounded-md bg-white button bg-white text-black text-sm font-bold relative cursor-pointer hover:-translate-y-0.5 transition duration-200 inline-block text-center';
 
   const variantStyles = {
-    primary: "",
-    secondary: "bg-transparent shadow-none dark:text-white",
-    dark: "bg-black text-white",
+    primary: '',
+    secondary: 'bg-transparent shadow-none dark:text-white',
+    dark: 'bg-black text-white',
     gradient:
-      "bg-gradient-to-b from-blue-500 to-blue-700 text-white shadow-[0px_2px_0px_0px_rgba(255,255,255,0.3)_inset]",
+      'bg-gradient-to-b from-blue-500 to-blue-700 text-white shadow-[0px_2px_0px_0px_rgba(255,255,255,0.3)_inset]',
   };
 
   return (
-    <Tag
-      href={href || undefined}
-      className={cn(baseStyles, variantStyles[variant], className)}
-      {...props}
-    >
+    <Tag href={href || undefined} className={cn(baseStyles, variantStyles[variant], className)} {...props}>
       {children}
     </Tag>
   );
